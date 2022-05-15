8e70aca46b544fcc512dd30f6ad62c3725342969
ankatus <antti.j.kataja@gmail.com>
2022-05-15 15:13:39 +0300

    Added some cli functionality

---
+++ Compiler/Cli.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+
+namespace Compiler;
+
+public static class Cli
+{
+    public static void Run(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Expected two arguments: INPUT_PATH, OUTPUT_PATH");
+            Environment.Exit(1);            
+        }
+
+        var inputPath = args[0];
+
+        var fileString = File.ReadAllText(inputPath);
+
+        var scannerInput = new ScannerInput(fileString);
+        var scanner = new Scanner(scannerInput);
+        var tokens = scanner.Scan();
+        
+        var parser = new Parser(tokens);
+        var parseTree = parser.Parse();
+
+        var analyzer = new Analyzer(parseTree);
+        var ast = analyzer.Analyze();
+
+        var generator = new Generator(ast);
+        var output = generator.Generate();
+
+        var outputPath = args[1];
+        File.WriteAllText(outputPath, output);
+        
+        Console.WriteLine($"Compiled program saved to {outputPath}");
+    }
+}
